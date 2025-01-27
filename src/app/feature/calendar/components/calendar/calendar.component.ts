05ad65fcf8f16c18f58bbbae30126031f844e840
iamdave86 <5939607+iamdave86@users.noreply.github.com>
2025-01-27 22:45:53 +0800

    feat: add Calendar service skeleton

--- src/app/feature/calendar/components/calendar/calendar.component.ts
+++ src/app/feature/calendar/components/calendar/calendar.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { CalendarService } from '@feature/calendar/services/calendar.service';
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CalendarComponent {}
+export class CalendarComponent {
+  constructor(private calendarService: CalendarService) {}
+}
