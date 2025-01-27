d26fa6c291d9633a8c06d2498de4c97813838900
iamdave86 <5939607+iamdave86@users.noreply.github.com>
2025-01-27 22:50:18 +0800

    feat(calendar.service): store weekday names with date-fns

--- src/app/feature/calendar/services/calendar.service.ts
+++ src/app/feature/calendar/services/calendar.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@angular/core';
+import { eachDayOfInterval, endOfWeek, format, startOfWeek } from 'date-fns';
 
 @Injectable()
-export class CalendarService {}
+export class CalendarService {
+  private weekDaysNames: string[];
+
+  constructor() {
+    this.weekDaysNames = this.createWeekDayNames();
+  }
+
+  public getWeekDayNames(): string[] {
+    return this.weekDaysNames;
+  }
+
+  private createWeekDayNames(): string[] {
+    return eachDayOfInterval({
+      start: startOfWeek(new Date(), { weekStartsOn: 0 }),
+      end: endOfWeek(new Date(), { weekStartsOn: 0 }),
+    }).map(date => format(date, 'EEEE'));
+  }
+}
